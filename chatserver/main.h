e5dd2495e7ebb784638babf84cb8ef130ead7ed3
loganb7869 <lpb67@nau.edu>
2025-11-15 14:35:36 -0700

    main files

---
+++ chatserver/main.h
@@ -0,0 +1,36 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+// other header files
+#include "properties.h"
+#include "sender_handler.h"
+#include "receiver_handler.h"
+#include "message.h"
+
+// standard libraries
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+// global constants
+#define SERVER_IP "127.0.0.1"
+#define SERVER_PORT 9000
+#define CLIENT_NAME 32
+
+// client struct
+typedef struct
+{
+	// store server ip
+	char server_ip[50];
+
+	// store port
+	uint16_t port;
+
+	// store client name
+	char name[CLIENT_NAME];
+} ClientProperties;
+
+#endif /* main.h */
